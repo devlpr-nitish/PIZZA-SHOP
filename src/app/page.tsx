5306be482a3573c2fbaca07da6784741fc5a0f79
Nitish <nk3227610@gmail.com>
2024-11-05 23:08:46 +0530

    project color added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
 
       Hello Welcome to Pizza Shop
-      <Button>Click me</Button>
+      <Button>Hello</Button>
     </div>
   );
 }
